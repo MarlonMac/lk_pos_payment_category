e34e386208b4558c5636b1f34c8bdb78b8a4b766
Marlon Macario <marlonmusician@gmail.com>
2024-08-30 12:22:54 -0600

    Se agregó la vista pos_payment_method_view.xml para mostrar correctamente la selección en el formulario de metodo de pago

--- __manifest__.py
+++ __manifest__.py
@@ -15,6 +15,7 @@
         'views/lk_pos_payment_category_view_tree.xml',
         'views/lk_pos_payment_category_view_form.xml',
         'views/pos_config_view_form.xml'
+        'views/pos_payment_method_view_form.xml'
     ],
     'qweb': [
         'static/src/xml/pos.xml'
